60c718add0b5797f17f378f6ec6293afeeffe0a1
sunjabag507-commits <sunjabag507@gmail.com>
2026-03-10 22:41:24 +0900

    main.py

--- main.py
+++ main.py
@@ -13,10 +13,12 @@ def home():
     return "I am alive"
 
 def run():
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT",10000))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
+    t.daemon = True
     t.start()
 
 # 봇 설정
